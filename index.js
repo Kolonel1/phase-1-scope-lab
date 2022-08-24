2a44eb8dd58acf81cf99256fd983672fcf32434b
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-24 16:24:50 +0300

    incomplete assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+   
+   return customerName.toUpperCase()
+  
+   }
+console.log(customerName);
+upperCaseCustomerName();
+
+
+function setBestCustomer(){ 
+    bestCustomer = 'not bob'
+    return bestCustomer
+};
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob'
+     return bestCustomer
+}
+
+
+
+//what do they mean by writing a variable with a global scope in a function
+const leastFavoriteCustomer = 'not rex'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='change';
+    console.log('hope it works.')
+}
+// i have seen a lot of stuff that i do not understand for shit
+// i tried and tried but still did not get it 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
